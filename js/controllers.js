756dd8b35a6007d26468cf5f0b97655b09bb3ef2
jayljill <jayljill@jayljillde-MacBook-Pro.local>
2017-07-31 16:33:14 +0800

    update images and modal

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,72 @@
-'use strict';
-
-
 var App = angular.module("App", ['ui.bootstrap']);
 
-App.controller("FirstCtrl", function($scope, $uibModal) {
-
-    $scope.openanta = function() {
+App.controller("FirstCtrl", function ($scope, $uibModal) {
+    $scope.openanta = function () {
         var modalInstance = $uibModal.open({
-            // animation: $ctrl.animationsEnabled,
-            // ariaLabelledBy: 'modal-title',
-            // ariaDescribedBy: 'modal-body',
-            // templateUrl: 'myModalContent.html',
-            template: '<h1>HIII</h1>',
-            // controller: function() {
-            //     alert(123);
-            // }
-        });
+            templateUrl: 'myModalContent.html',
+            windowClass: 'app-modal-window',
+            controller: function ($scope) {
+                $scope.myInterval = 5000;
+                $scope.noWrapSlides = false;
+                $scope.active = 0;
+                var slides = $scope.slides = [];
+                var currIndex = 0;
 
-        modalInstance.result.then(function() {
+                $scope.addSlide = function (i) {
+                    slides.push({
+                        image: './images/' + i +'.png',
+                        text: 'demo',
+                        id: currIndex++
+                    });
+                };
+
+                $scope.randomize = function () {
+                    var indexes = generateIndexesArray();
+                    assignNewIndexesToSlides(indexes);
+                };
+
+                for (var i = 0; i < 4; i++) {
+                    $scope.addSlide(i+1);
+                }
+
+                // Randomize logic below
+
+                function assignNewIndexesToSlides(indexes) {
+                    for (var i = 0, l = slides.length; i < l; i++) {
+                        slides[i].id = indexes.pop();
+                    }
+                }
+
+                function generateIndexesArray() {
+                    var indexes = [];
+                    for (var i = 0; i < currIndex; ++i) {
+                        indexes[i] = i;
+                    }
+                    return shuffle(indexes);
+                }
+
+                // http://stackoverflow.com/questions/962802#962890
+                function shuffle(array) {
+                    var tmp, current, top = array.length;
+
+                    if (top) {
+                        while (--top) {
+                            current = Math.floor(Math.random() * (top + 1));
+                            tmp = array[current];
+                            array[current] = array[top];
+                            array[top] = tmp;
+                        }
+                    }
+
+                    return array;
+                }
+            }
+        });
+        modalInstance.result.then(function () {
             console.log('yes');
-        }, function() {
+        }, function () {
             console.log('Modal dismissed at: ' + new Date());
         });
     };
 
 });
-
