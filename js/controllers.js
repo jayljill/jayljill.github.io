54d0cd125c8b0547302a79aeea406a8e592e4580
jayljill <jayljill@jayljillde-MacBook-Pro.local>
2017-08-11 17:13:43 +0800

    update portfolio and content

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,7 @@ var App = angular.module("App", ['ui.bootstrap']);
 App.controller("FirstCtrl", function ($scope, $uibModal) {
     $scope.openanta = function () {
         var modalInstance = $uibModal.open({
-            templateUrl: 'myModalContent.html',
+            templateUrl: 'myModalContent1.html',
             windowClass: 'app-modal-window',
             controller: function ($scope) {
                 $scope.myInterval = 5000;
@@ -69,10 +69,9 @@ App.controller("FirstCtrl", function ($scope, $uibModal) {
         });
     };
 
-
     $scope.openmozila = function () {
         var modalInstance = $uibModal.open({
-            templateUrl: 'myModalContent.html',
+            templateUrl: 'myModalContent2.html',
             windowClass: 'app-modal-window',
             controller: function ($scope) {
                 $scope.myInterval = 5000;
@@ -94,7 +93,7 @@ App.controller("FirstCtrl", function ($scope, $uibModal) {
                     assignNewIndexesToSlides(indexes);
                 };
 
-                for (var i = 4; i < 9; i++) {
+                for (var i = 4; i < 10; i++) {
                     $scope.addSlide(i + 1);
                 }
 
@@ -137,7 +136,279 @@ App.controller("FirstCtrl", function ($scope, $uibModal) {
             console.log('Modal dismissed at: ' + new Date());
         });
     };
-    
+
+    $scope.opensharp = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'myModalContent3.html',
+            windowClass: 'app-modal-window',
+            controller: function ($scope) {
+                $scope.myInterval = 5000;
+                $scope.noWrapSlides = false;
+                $scope.active = 0;
+                var slides = $scope.slides = [];
+                var currIndex = 0;
+
+                $scope.addSlide = function (i) {
+                    slides.push({
+                        image: './images/' + i + '.png',
+                        text: 'demo',
+                        id: currIndex++
+                    });
+                };
+
+                $scope.randomize = function () {
+                    var indexes = generateIndexesArray();
+                    assignNewIndexesToSlides(indexes);
+                };
+
+                for (var i = 10; i < 13; i++) {
+                    $scope.addSlide(i + 1);
+                }
+
+                // Randomize logic below
+
+                function assignNewIndexesToSlides(indexes) {
+                    for (var i = 0, l = slides.length; i < l; i++) {
+                        slides[i].id = indexes.pop();
+                    }
+                }
+
+                function generateIndexesArray() {
+                    var indexes = [];
+                    for (var i = 0; i < currIndex; ++i) {
+                        indexes[i] = i;
+                    }
+                    return shuffle(indexes);
+                }
+
+                // http://stackoverflow.com/questions/962802#962890
+                function shuffle(array) {
+                    var tmp, current, top = array.length;
+
+                    if (top) {
+                        while (--top) {
+                            current = Math.floor(Math.random() * (top + 1));
+                            tmp = array[current];
+                            array[current] = array[top];
+                            array[top] = tmp;
+                        }
+                    }
+
+                    return array;
+                }
+            }
+        });
+        modalInstance.result.then(function () {
+            console.log('yes');
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.openpinterest = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'myModalContent4.html',
+            windowClass: 'app-modal-window',
+            controller: function ($scope) {
+                $scope.myInterval = 5000;
+                $scope.noWrapSlides = false;
+                $scope.active = 0;
+                var slides = $scope.slides = [];
+                var currIndex = 0;
+
+                $scope.addSlide = function (i) {
+                    slides.push({
+                        image: './images/' + i + '.png',
+                        text: 'demo',
+                        id: currIndex++
+                    });
+                };
+
+                $scope.randomize = function () {
+                    var indexes = generateIndexesArray();
+                    assignNewIndexesToSlides(indexes);
+                };
+
+                for (var i = 13; i < 21; i++) {
+                    $scope.addSlide(i + 1);
+                }
+
+                // Randomize logic below
+
+                function assignNewIndexesToSlides(indexes) {
+                    for (var i = 0, l = slides.length; i < l; i++) {
+                        slides[i].id = indexes.pop();
+                    }
+                }
+
+                function generateIndexesArray() {
+                    var indexes = [];
+                    for (var i = 0; i < currIndex; ++i) {
+                        indexes[i] = i;
+                    }
+                    return shuffle(indexes);
+                }
+
+                // http://stackoverflow.com/questions/962802#962890
+                function shuffle(array) {
+                    var tmp, current, top = array.length;
+
+                    if (top) {
+                        while (--top) {
+                            current = Math.floor(Math.random() * (top + 1));
+                            tmp = array[current];
+                            array[current] = array[top];
+                            array[top] = tmp;
+                        }
+                    }
+
+                    return array;
+                }
+            }
+        });
+        modalInstance.result.then(function () {
+            console.log('yes');
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.openbreakfast = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'myModalContent5.html',
+            windowClass: 'app-modal-window',
+            controller: function ($scope) {
+                $scope.myInterval = 5000;
+                $scope.noWrapSlides = false;
+                $scope.active = 0;
+                var slides = $scope.slides = [];
+                var currIndex = 0;
+
+                $scope.addSlide = function (i) {
+                    slides.push({
+                        image: './images/' + i + '.png',
+                        text: 'demo',
+                        id: currIndex++
+                    });
+                };
+
+                $scope.randomize = function () {
+                    var indexes = generateIndexesArray();
+                    assignNewIndexesToSlides(indexes);
+                };
+
+                for (var i = 21; i < 23; i++) {
+                    $scope.addSlide(i + 1);
+                }
+
+                // Randomize logic below
+
+                function assignNewIndexesToSlides(indexes) {
+                    for (var i = 0, l = slides.length; i < l; i++) {
+                        slides[i].id = indexes.pop();
+                    }
+                }
+
+                function generateIndexesArray() {
+                    var indexes = [];
+                    for (var i = 0; i < currIndex; ++i) {
+                        indexes[i] = i;
+                    }
+                    return shuffle(indexes);
+                }
+
+                // http://stackoverflow.com/questions/962802#962890
+                function shuffle(array) {
+                    var tmp, current, top = array.length;
+
+                    if (top) {
+                        while (--top) {
+                            current = Math.floor(Math.random() * (top + 1));
+                            tmp = array[current];
+                            array[current] = array[top];
+                            array[top] = tmp;
+                        }
+                    }
+
+                    return array;
+                }
+            }
+        });
+        modalInstance.result.then(function () {
+            console.log('yes');
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.opennokia = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'myModalContent5.html',
+            windowClass: 'app-modal-window',
+            controller: function ($scope) {
+                $scope.myInterval = 5000;
+                $scope.noWrapSlides = false;
+                $scope.active = 0;
+                var slides = $scope.slides = [];
+                var currIndex = 0;
+
+                $scope.addSlide = function (i) {
+                    slides.push({
+                        image: './images/' + i + '.png',
+                        text: 'demo',
+                        id: currIndex++
+                    });
+                };
+
+                $scope.randomize = function () {
+                    var indexes = generateIndexesArray();
+                    assignNewIndexesToSlides(indexes);
+                };
+
+                for (var i = 23; i < 25; i++) {
+                    $scope.addSlide(i + 1);
+                }
+
+                // Randomize logic below
+
+                function assignNewIndexesToSlides(indexes) {
+                    for (var i = 0, l = slides.length; i < l; i++) {
+                        slides[i].id = indexes.pop();
+                    }
+                }
+
+                function generateIndexesArray() {
+                    var indexes = [];
+                    for (var i = 0; i < currIndex; ++i) {
+                        indexes[i] = i;
+                    }
+                    return shuffle(indexes);
+                }
+
+                // http://stackoverflow.com/questions/962802#962890
+                function shuffle(array) {
+                    var tmp, current, top = array.length;
+
+                    if (top) {
+                        while (--top) {
+                            current = Math.floor(Math.random() * (top + 1));
+                            tmp = array[current];
+                            array[current] = array[top];
+                            array[top] = tmp;
+                        }
+                    }
+
+                    return array;
+                }
+            }
+        });
+        modalInstance.result.then(function () {
+            console.log('yes');
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
     $scope.moreinfo = function () {
         $scope.tobyShow = true;
         console.log($scope.tobyShow);
